152ec334f70b1a24c01d9a6cefac688fb1619dd3
Himanshu <himanshu16315@gmail.com>
2023-01-29 11:34:21 -0500

    Added Cloud to game scene

---
+++ Objects/Cloud.swift
@@ -0,0 +1,63 @@
+//
+//  Cloud.swift
+//  MAPD724-ICE1
+//
+//  Created by Himanshu on 2023-01-29.
+//
+
+import GameplayKit
+import SpriteKit
+
+class Cloud : GameObject {
+    // initializer / constructor
+    init() {
+        super.init(imageString: "cloud", initialScale: 1.0)
+        Start()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // LifeCylce Functions
+    override func Start() {
+        zPosition = Layers.cloud.rawValue
+        alpha = 0.5 // 50% transparent
+        Reset()
+    }
+    
+    override func Update() {
+        Move()
+        CheckBounds()
+    }
+    
+    override func CheckBounds() {
+        if(position.y <= -902)
+        {
+            Reset()
+        }
+    }
+    
+    override func Reset() {
+        // randomize the vertical speed
+        verticalSpeed = CGFloat((randomSource?.nextUniform())! * 5.0) + 5.0
+        
+        // randomize the horizontal speed
+        horizontalSpeed = CGFloat((randomSource?.nextUniform())! * 4.0) - 2.0
+        
+        // get a pseudo random number -313 to 313
+        let randomX:Int = (randomSource?.nextInt(upperBound: 524))! - 262
+        position.x = CGFloat(randomX)
+        
+        // get a pseudo random number 902 to 932
+        let randomY:Int = (randomSource?.nextInt(upperBound: 30))! + 902
+        position.y = CGFloat(randomY)
+        
+        isColliding = false
+    }
+    
+    func Move() {
+        position.x -= horizontalSpeed!
+        position.y -= verticalSpeed!
+    }
+}
